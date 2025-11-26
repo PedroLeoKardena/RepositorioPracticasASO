c7cac519d1cdf5f0c2faf70e583654d84652935e
Manuel <manuel.c.p@um.es>
2025-11-26 12:10:10 +0100

    Exit y wait

--- xv6/trap.c
+++ xv6/trap.c
@@ -102,8 +102,13 @@ trap(struct trapframe *tf)
   // Force process exit if it has been killed and is in user space.
   // (If it is still executing in the kernel, let it keep running
   // until it gets to the regular system call return.)
-  if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
-    exit();
+  if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER){
+    myproc()->exitstatus = ((tf->trapno + 1) & 0x7f);   // Codigo de salida por trap+1 con los 7 menos significativos
+    myproc()->killed = 1;
+    if((tf->cs & 3) == DPL_USER)
+      exit();
+  }
+ 
 
   // Force process to give up CPU on clock tick.
   // If interrupts were on while locks held, would need to check nlock.
